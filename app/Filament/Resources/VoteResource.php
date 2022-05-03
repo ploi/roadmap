35ece0128b16507f8b8cbb20addf85c35b783ed2
Dennis <dennissmink@gmail.com>
2022-05-03 19:36:13 +0200

    wip

---
+++ app/Filament/Resources/VoteResource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VoteResource\Pages;
+use App\Filament\Resources\VoteResource\RelationManagers;
+use App\Models\Vote;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class VoteResource extends Resource
+{
+    protected static ?string $model = Vote::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-thumb-up';
+
+    protected static ?string $navigationGroup = 'Manage';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name'),
+                Tables\Columns\TextColumn::make('item.title'),
+                Tables\Columns\TextColumn::make('created_at')->dateTime()->sortable()->label('Date'),
+            ])
+            ->filters([
+                //
+            ])
+            ->defaultSort('created_at', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVotes::route('/'),
+            'create' => Pages\CreateVote::route('/create'),
+            'edit' => Pages\EditVote::route('/{record}/edit'),
+        ];
+    }
+}
