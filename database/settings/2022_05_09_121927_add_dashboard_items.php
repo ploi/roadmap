b2070409b4724b7f399916fccc00ccee615f9a26
Dennis <dennissmink@gmail.com>
2022-05-12 10:18:16 +0200

    fx

--- database/settings/2022_05_09_121927_add_dashboard_items.php
+++ database/settings/2022_05_09_121927_add_dashboard_items.php
@@ -6,10 +6,7 @@ class AddDashboardItems extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('general.dashboard_items', [
-            'recent-items',
-            'recent-comments'
-        ]);
+        $this->migrator->add('general.dashboard_items', []);
 
         $this->migrator->add('general.welcome_text', 'Welcome to our roadmap!');
     }
