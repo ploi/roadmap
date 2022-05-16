06b9140ee9c309f449d311cff914367d340a5136
Dennis <dennissmink@gmail.com>
2022-05-16 17:12:50 +0200

    Merge settings into 1

--- database/settings/2022_05_03_121713_create_general_settings.php
+++ database/settings/2022_05_03_121713_create_general_settings.php
@@ -6,6 +6,18 @@ class CreateGeneralSettings extends SettingsMigration
 {
     public function up(): void
     {
+        // General
         $this->migrator->add('general.board_centered', false);
+        $this->migrator->add('general.create_default_boards', true);
+        $this->migrator->add('general.default_boards', [
+            'Planned', 'In progress', 'Live'
+        ]);
+        $this->migrator->add('general.show_projects_sidebar_without_boards', true);
+        $this->migrator->add('general.allow_general_creation_of_item', true);
+        $this->migrator->add('general.dashboard_items', []);
+        $this->migrator->add('general.welcome_text', 'Welcome to our roadmap!');
+
+        // Colors
+        $this->migrator->add('colors.primary', '#2563EB');
     }
 }
