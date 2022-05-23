e0af4587ef2915b4b79a35cc0110f839b2534a12
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-05-23 19:40:33 +0200

    WIP

--- app/Observers/ItemObserver.php
+++ app/Observers/ItemObserver.php
@@ -3,7 +3,10 @@
 namespace App\Observers;
 
 use App\Mail\Admin\ItemHasBeenCreatedEmail;
+use App\Models\Board;
 use App\Models\Item;
+use App\Models\User;
+use App\Notifications\Item\ItemUpdatedNotification;
 use App\Settings\GeneralSettings;
 use Mail;
 
@@ -19,6 +22,35 @@ class ItemObserver
             foreach($receivers as $receiver){
                 Mail::to($receiver['email'])->send(new ItemHasBeenCreatedEmail($receiver, $item));
             }
+        }
+    }
+
+    public function updating(Item $item)
+    {
+        $isDirty = false;
+
+        if ($item->isDirty('board_id')) {
+            activity()
+                ->performedOn($item)
+                ->log('moved item to board ' . $item->board->title);
+
+            $isDirty = true;
+        }
+
+        if ($item->isDirty('project_id')) {
+            activity()
+                ->performedOn($item)
+                ->log('moved item to project ' . $item->project->title);
+
+            $isDirty = true;
+        }
+
+        if ($isDirty) {
+            $users = $item->subscribedVotes()->with('user')->get()->pluck('user');
+
+            $users->each(function (User $user) use ($item) {
+                $user->notify(new ItemUpdatedNotification($item));
+            });
         }
     }
 
