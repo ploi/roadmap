6431209e9cfe2be7f46728f81c47a250f3b760de
Dennis <dennissmink@gmail.com>
2022-05-23 09:11:21 +0200

    wip

--- resources/views/livewire/board/item-card.blade.php
+++ resources/views/livewire/board/item-card.blade.php
@@ -1,7 +1,7 @@
-<li class="pb-5 pt-5 first:pt-0">
+<li class="pb-5 pt-5 first:pt-0 group">
     <div class="inline-block flex space-x-3">
         <div class="flex flex-col text-center -space-y-1">
-            <button wire:click="toggleUpvote">
+            <button wire:click="toggleUpvote" class="hover:text-primary-500">
                 <x-heroicon-o-chevron-up class="w-5 h-5" />
             </button>
 
@@ -9,7 +9,7 @@
         </div>
 
         <a href="{{ route('projects.items.show', [$project->id, $item->id]) }}" class="flex-1">
-            <p class="font-bold text-lg">{{ $item->title }}</p>
+            <p class="font-bold text-lg group-hover:text-primary-500">{{ $item->title }}</p>
             <p>{{ $item->excerpt }}</p>
         </a>
 
