b40a78e9a79e79942710db009f90c098744237ef
Dennis <dennissmink@gmail.com>
2022-05-24 14:58:15 +0200

    wip

--- app/Http/Livewire/Welcome/RecentItems.php
+++ app/Http/Livewire/Welcome/RecentItems.php
@@ -28,14 +28,14 @@ class RecentItems extends Component implements HasTable
     {
         return function ($record) {
             if (!$record->board) {
-                return route('items.show', $record->id);
+                return route('items.show', $record);
             }
 
             if (!$record->project) {
-                return route('items.show', $record->id);
+                return route('items.show', $record);
             }
 
-            return route('projects.items.show', [$record->project->id, $record->id]);
+            return route('projects.items.show', [$record->project, $record]);
         };
     }
 
