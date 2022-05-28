9739929193e5c21bf6496e9bf469f521f9dd8be3
Dennis <dennissmink@gmail.com>
2022-05-28 14:32:48 +0200

    Improved OG image API service class

---
+++ app/Services/OgImageGenerator.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Services;
+
+use App\Settings\ColorSettings;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
+
+class OgImageGenerator
+{
+    public string $title;
+    public string $subject;
+    public string $description;
+    public string|null $templateFile = null;
+    public string $imageName;
+    public bool $enablePolygon = false;
+    public array $polygonPoints;
+
+    public function setTitle(string $title): static
+    {
+        $this->title = Str::limit($title, 28);
+
+        return $this;
+    }
+
+    public function setSubject(string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function setImageTemplateFile(string $templateFile): static
+    {
+        $this->templateFile = $templateFile;
+
+        return $this;
+    }
+
+    public function setImageName(string $imageName = null)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function polygon($status = true, $points = []): static
+    {
+        $this->enablePolygon = $status;
+        $this->polygonPoints = $points ?: [
+            1200, 200,
+            1200, 630,
+            825, 630,
+        ];
+
+        return $this;
+    }
+
+    public function getImageName(): string
+    {
+        return $this->imageName ?? 'og-' . md5(time()) . '.jpg';
+    }
+
+    public function getImagePath()
+    {
+        return storage_path('app/public/' . $this->getImageName());
+    }
+
+    public function generateImage()
+    {
+        $pathToImage = $this->getImagePath();
+
+        if (!File::exists($pathToImage)) {
+            $image = Image::canvas(1200, 627, '#ffffff');
+
+            if ($this->templateFile) {
+                $image = Image::make($this->templateFile);
+            }
+
+            $brandingColors = $this->getBrandingColors();
+
+            $wordCount = str_word_count($this->title);
+            $y = 270;
+
+            if ($wordCount > 9) {
+                $y = 240;
+            }
+
+            // Set subject
+            if ($this->subject) {
+                $image->text(wordwrap($this->subject, 22, PHP_EOL), 78, $y - 65, function ($font) use ($brandingColors) {
+                    $font->file(public_path('fonts/Lexend-Bold.ttf'));
+                    $font->size(25);
+                    $font->color($brandingColors->shades['brand-500']);
+                });
+            }
+
+            // Set title
+            $image->text(Str::limit($this->title, 28), 75, $y, function ($font) {
+                $font->file(public_path('fonts/Lexend-Bold.ttf'));
+                $font->size(60);
+                $font->color('#000');
+            });
+
+            if ($this->description) {
+                $image->text(wordwrap($this->description, 35, PHP_EOL), 75, $y + 60, function ($font) {
+                    $font->file(public_path('fonts/Lexend-SemiBold.ttf'));
+                    $font->size(30);
+                    $font->color('#000');
+                });
+            }
+
+            if ($this->enablePolygon) {
+                $image->polygon($this->polygonPoints, function ($draw) use ($brandingColors) {
+                    $draw->background($brandingColors->shades['brand-100']);
+                });
+            }
+
+            $image->save($pathToImage);
+        }
+
+        return asset($pathToImage);
+    }
+
+    protected function getBrandingColors(): Tailwind
+    {
+        return new Tailwind('brand', app(ColorSettings::class)->primary);
+    }
+}
--- app/Traits/HasOgImage.php
+++ app/Traits/HasOgImage.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use App\Services\OgImageGenerator;
 use App\Services\Tailwind;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
@@ -11,60 +12,13 @@ trait HasOgImage
 {
     public function getOgImage($withDescription = false, $subject = 'Roadmap')
     {
-        $pathToImage = storage_path('app/public/og-' . $this->slug . '-' . $this->id . '.jpg');
+        $service = (new OgImageGenerator())
+            ->setSubject($subject)
+            ->setTitle($this->title)
+            ->setDescription($withDescription)
+            ->polygon()
+            ->setImageName($this->slug . '-' . $this->id . '.jpg');
 
-        if (!File::exists($pathToImage)) {
-            try {
-                $tailwind = new Tailwind('brand', app(\App\Settings\ColorSettings::class)->primary);
-
-                $wordCount = str_word_count($this->title);
-                $y = 270;
-
-                if ($wordCount > 9) {
-                    $y = 240;
-                }
-
-                $img = Image::make(public_path('images/og-template.jpg'));
-                if ($subject) {
-                    $img->text(wordwrap($subject, 22, PHP_EOL), 78, $y - 65, function ($font) use ($tailwind) {
-                        $font->file(public_path('fonts/Lexend-Bold.ttf'));
-                        $font->size(25);
-                        $font->color($tailwind->shades['brand-500']);
-                    });
-                }
-
-                $img->text(wordwrap(trim(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', mb_convert_encoding(Str::limit($this->title, 15), "UTF-8"))), 22, PHP_EOL), 75, $y, function ($font) {
-                    $font->file(public_path('fonts/Lexend-Bold.ttf'));
-                    $font->size(60);
-                    $font->color('#000');
-                });
-
-                if ($withDescription) {
-                    $img->text(wordwrap($withDescription, 35, PHP_EOL), 75, $y + 60, function ($font) {
-                        $font->file(public_path('fonts/Lexend-SemiBold.ttf'));
-                        $font->size(30);
-                        $font->color('#000');
-                    });
-                }
-
-                $points = [
-                    1200,  200,
-                    1200,  630,
-                    825,  630,
-                ];
-
-                $img->polygon($points, function ($draw) use ($tailwind) {
-                    $draw->background($tailwind->shades['brand-100']);
-                });
-
-                $img->save($pathToImage);
-
-                return asset('storage/og-' . $this->slug . '-' . $this->id . '.jpg?v=' . $this->updated_at->timestamp);
-            } catch (\Throwable $exception) {
-                return asset('images/og-template.jpg');
-            }
-        }
-
-        return asset('storage/og-' . $this->slug . '-' . $this->id . '.jpg?v=' . $this->updated_at->timestamp);
+        return $service->generateImage();
     }
 }
