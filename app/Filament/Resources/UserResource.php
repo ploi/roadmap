7e63034662c944730f9671be9913790862d92797
Dennis <dennissmink@gmail.com>
2022-05-30 21:11:30 +0200

    search in admin for users

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -32,8 +32,8 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('email')->searchable(),
                 Tables\Columns\TextColumn::make('created_at')->sortable()->dateTime()->label('Date'),
             ])
             ->filters([
