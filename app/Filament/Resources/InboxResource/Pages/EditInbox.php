7ab722a084ffbc2c5a673abbb268606b617332d4
Dennis <dennissmink@gmail.com>
2022-05-31 09:40:04 +0200

    wip

--- app/Filament/Resources/InboxResource/Pages/EditInbox.php
+++ app/Filament/Resources/InboxResource/Pages/EditInbox.php
@@ -4,8 +4,17 @@ namespace App\Filament\Resources\InboxResource\Pages;
 
 use App\Filament\Resources\InboxResource;
 use App\Filament\Resources\ItemResource\Pages\EditItem;
+use Filament\Pages\Actions\Action;
 
 class EditInbox extends EditItem
 {
     protected static string $resource = InboxResource::class;
+
+    public function getActions(): array
+    {
+        return [
+            Action::make('view_public')->color('secondary')->url(fn() => route('items.show', $this->record)),
+            ...parent::getActions()
+        ];
+    }
 }
--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -15,14 +15,16 @@
             <x-card>
                 <header class="flex items-center px-4 py-2 space-x-4">
                     <div class="flex items-center flex-1 space-x-3 overflow-hidden">
-                        <div class="relative flex-shrink-0 w-10 h-10 rounded-full">
-                            <img class="absolute inset-0 object-cover rounded-full"
-                                 src="{{ $user->getGravatar() }}"
-                                 alt="{{ $user->name }}">
-                        </div>
+                        @if($user)
+                            <div class="relative flex-shrink-0 w-10 h-10 rounded-full">
+                                <img class="absolute inset-0 object-cover rounded-full"
+                                     src="{{ $user->getGravatar() }}"
+                                     alt="{{ $user->name }}">
+                            </div>
+                        @endif
 
                         <div class="overflow-hidden font-medium">
-                            <p>{{ $user->name }}</p>
+                            <p>{{ $user->name ?? '-Unknown user-' }}</p>
                         </div>
 
                         @if($item->board)
@@ -94,7 +96,7 @@
                             <div>
                                 <p class="text-sm font-medium">
                                     <span class="font-semibold">
-                                        {{ $activity->causer->name }}
+                                        {{ $activity->causer->name ?? 'Unknown user' }}
                                     </span>
                                     {{ $activity->description }}
                                 </p>
