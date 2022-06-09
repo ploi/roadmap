bfcac2565bf231057b64088d3a0027effd1a1e2e
Dennis <dennissmink@gmail.com>
2022-06-09 08:08:50 +0200

    View public to projects resource

--- app/Filament/Resources/ProjectResource/Pages/EditProject.php
+++ app/Filament/Resources/ProjectResource/Pages/EditProject.php
@@ -2,10 +2,19 @@
 
 namespace App\Filament\Resources\ProjectResource\Pages;
 
+use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 use App\Filament\Resources\ProjectResource;
 
 class EditProject extends EditRecord
 {
     protected static string $resource = ProjectResource::class;
+
+    public function getActions(): array
+    {
+        return [
+            Action::make('view_public')->color('secondary')->url(fn() => route('projects.show', $this->record)),
+            ...parent::getActions()
+        ];
+    }
 }
