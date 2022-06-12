c16d1ae890211c105d0f0cd01d3de61c121bce89
Silver343 <51054165+Silver343@users.noreply.github.com>
2022-06-12 17:40:53 +0100

    add pin icon

--- resources/views/livewire/board/item-card.blade.php
+++ resources/views/livewire/board/item-card.blade.php
@@ -1,11 +1,14 @@
 <li class="pb-5 pt-5 first:pt-0 group">
-    <div class="inline-block flex space-x-3">
+    <div class="flex space-x-3">
         <div class="flex flex-col text-center -space-y-1">
             <button wire:click="toggleUpvote" class="hover:text-primary-500">
                 <x-heroicon-o-chevron-up class="w-5 h-5" />
             </button>
 
             <span class="">{{ $item->total_votes }}</span>
+            @if($item->isPinned())
+            {{-- pin svg--}}<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M15 11.586V6h2V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2h2v5.586l-2.707 1.707A.996.996 0 0 0 6 14v2a1 1 0 0 0 1 1h4v3l1 2 1-2v-3h4a1 1 0 0 0 1-1v-2a.996.996 0 0 0-.293-.707L15 11.586z"></path></svg>
+            @endif
         </div>
 
         <a href="{{ route('projects.items.show', [$project, $item]) }}" class="flex-1">
