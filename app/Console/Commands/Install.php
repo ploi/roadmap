f9a576f5ef1c6c8a43fe4815b3b104549f4ed684
Dennis <dennissmink@gmail.com>
2022-06-21 13:13:38 +0200

    Closes #67

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -17,32 +17,97 @@ class Install extends Command
 
     public function handle()
     {
-        $user = $this->createUser();
-        $user->admin = true;
-        $user->save();
-
-        $this->info('User created!');
-
+        $this->intro();
+        $this->refreshDatabase();
+        $this->createUser();
+        $this->linkStorage();
         $this->runNpm();
+
+        $this->askForStar();
+
+        $this->writeSeparationLine();
+        $this->line(' ');
 
         $this->info('All done! You can now login at ' . route('filament.auth.login'));
     }
 
+    protected function intro()
+    {
+        $this->writeSeparationLine();
+        $this->line('Roadmap Installation');
+        $this->line('Laravel version: ' . app()->version());
+        $this->line('PHP version: ' . trim(phpversion()));
+        $this->line(' ');
+        $this->line('Github: https://github.com/ploi-deploy/roadmap');
+        $this->writeSeparationLine();
+        $this->line('');
+    }
+
+    protected function refreshDatabase()
+    {
+        if ($this->confirm('Do you want to run the migrations to set up everything fresh? (php artisan migrate:fresh)')) {
+            $this->call('migrate:fresh');
+        }
+    }
+
     protected function createUser()
     {
-        return User::create($this->getUserData());
+        $this->info('Let\'s create a user.');
+
+        $user = User::create($this->getUserData());
+        $user->role = User::ROLE_ADMIN;
+        $user->save();
+
+        $this->info('User created!');
+
+        return $user;
+    }
+
+    protected function linkStorage()
+    {
+        if (!file_exists(public_path('storage')) && $this->confirm('Your storage does not seem to be linked, do you want me to do this?')) {
+            $this->call('storage:link');
+        }
     }
 
     protected function runNpm()
     {
+        if ($this->confirm('Do you want to run npm install & npm run production to get the assets ready?')) {
+            $this->info('Running NPM..');
+
+            shell_exec('npm install');
+            shell_exec('npm run production');
+
+            $this->info('NPM installation & mixing production done!');
+        }
+    }
+
+    protected function askForStar()
+    {
+        if (User::count() === 1 && $this->confirm('Would you like to show some love by starring the repo?', true)) {
+            if (PHP_OS_FAMILY === 'Darwin') {
+                exec('open https://github.com/ploi-deploy/roadmap');
+            }
+            if (PHP_OS_FAMILY === 'Linux') {
+                exec('xdg-open https://github.com/ploi-deploy/roadmap');
+            }
+            if (PHP_OS_FAMILY === 'Windows') {
+                exec('start https://github.com/ploi-deploy/roadmap');
+            }
+        }
     }
 
     protected function getUserData(): array
     {
         return [
-            'name' => $this->validateInput(fn () => $this->ask('Name'), 'name', ['required']),
-            'email' => $this->validateInput(fn () => $this->ask('Email address'), 'email', ['required', 'email', 'unique:' . User::class]),
-            'password' => Hash::make($this->validateInput(fn () => $this->secret('Password'), 'password', ['required', 'min:8'])),
+            'name' => $this->validateInput(fn() => $this->ask('Name'), 'name', ['required']),
+            'email' => $this->validateInput(fn() => $this->ask('Email address'), 'email', ['required', 'email', 'unique:' . User::class]),
+            'password' => Hash::make($this->validateInput(fn() => $this->secret('Password'), 'password', ['required', 'min:8'])),
         ];
+    }
+
+    protected function writeSeparationLine()
+    {
+        $this->info('*---------------------------------------------------------------------------*');
     }
 }
