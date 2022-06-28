27f3f6f1936d80e37ba6a210ae2038277ff1c901
Dennis <dennissmink@gmail.com>
2022-06-28 14:40:25 +0200

    wip

--- resources/views/livewire/changelog/index.blade.php
+++ resources/views/livewire/changelog/index.blade.php
@@ -1,4 +1,4 @@
-<ul class="w-full divide-y pr-10">
+<ul class="w-full space-y-8">
     @foreach($changelogs as $changelog)
         <livewire:changelog.item :changelog="$changelog"/>
     @endforeach
