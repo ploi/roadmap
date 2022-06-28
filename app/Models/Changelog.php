73c724bd2b91986a9e3d82e9aef2c3fd7b4b8b0d
Nick Dijkstra <3372841+ndijkstra@users.noreply.github.com>
2022-06-28 11:08:39 +0200

    Add tests

--- app/Models/Changelog.php
+++ app/Models/Changelog.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use App\Traits\HasOgImage;
 use App\Traits\Sluggable;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Support\Str;
 
 class Changelog extends Model
 {
--- database/factories/ChangelogFactory.php
+++ database/factories/ChangelogFactory.php
@@ -17,7 +17,18 @@ class ChangelogFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->words(3, true),
+            'slug' => $this->faker->word,
+            'content' => $this->faker->paragraphs(3, true),
         ];
+    }
+
+    public function published()
+    {
+        return $this->state(function () {
+            return [
+                'published_at' => now(),
+            ];
+        });
     }
 }
