2bf38acfe40a060845099ce22cb28822d2cd8248
Dennis Smink <dennissmink@mbp-van-dennis.lan>
2022-07-07 20:45:21 +0200

    wip

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/css/app.css',
+            'resources/css/admin.css',
+            'resources/js/app.js',
+        ]),
+    ],
+});
