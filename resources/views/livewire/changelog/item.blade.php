799bcbbea268324d397eaded818a5d04c6bc15ea
Dennis Smink <dennissmink@mbp-van-dennis.lan>
2022-07-21 09:29:32 +0200

    changelog improvements

--- resources/views/livewire/changelog/item.blade.php
+++ resources/views/livewire/changelog/item.blade.php
@@ -29,14 +29,13 @@
     @if(app(App\Settings\GeneralSettings::class)->show_changelog_related_items && $changelog->items->count())
         <div class="border-t border-gray-200 w-full py-2">
             <p class="font-semibold mb-2">{{ trans('changelog.included-items') }}</p>
-            <div class="space-x-2">
+            <div class="grid grid-cols-4 gap-x-4 gap-y-2">
                 @foreach($changelog->items as $item)
-                    <span
-                        class="inline-flex items-center justify-center h-6 px-2 text-sm font-semibold tracking-tight text-primary-800 rounded-md bg-primary-500/5 hover:bg-primary-700/10 shadow hover:scale-105">
-                        <a class="no-underline"
-                           title="{{ $item->title }}"
-                           href="{{ route('items.show', $item) }}">{{ $item->title }} {{ $item->project ? '(' . $item->project->title . ')' : '' }}</a>
-                    </span>
+                    <a title="{{ $item->title }}"
+                       href="{{ route('items.show', $item) }}"
+                        class="w-full flex items-center h-6 px-2 text-sm font-semibold tracking-tight text-primary-800 rounded-md bg-primary-500/5 hover:bg-primary-700/10 shadow hover:scale-[1.015]">
+                        <span class="no-underline truncate">{{ $item->title }} {{ $item->project ? '(' . $item->project->title . ')' : '' }}</span>
+                    </a>
                 @endforeach
             </div>
         </div>
