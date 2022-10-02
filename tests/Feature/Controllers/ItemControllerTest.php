17ada57851986fcf7660acfbc02aa12d630d4ab4
Nerijus <krekas@gmail.com>
2022-10-02 11:10:33 +0300

    Fix tests

--- tests/Feature/Controllers/ItemControllerTest.php
+++ tests/Feature/Controllers/ItemControllerTest.php
@@ -6,7 +6,7 @@ use App\Enums\UserRole;
 use App\Models\Project;
 use function Pest\Laravel\get;
 use App\Http\Livewire\Item\Comments;
-use App\Http\Livewire\Item\VoteButton2;
+use App\Http\Livewire\Item\VoteButton;
 use function Pest\Laravel\post;
 use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertSame;
@@ -60,7 +60,7 @@ test('view contains item.comments component', function () {
 test('view contains item.vote-button component', function () {
     $item = Item::factory()->create();
 
-    get(route('items.show', $item))->assertSeeLivewire(VoteButton2::class);
+    get(route('items.show', $item))->assertSeeLivewire(VoteButton::class);
 });
 
 test('user can not view private item', function (?UserRole $userRole, int $expectedStatusCode) {
