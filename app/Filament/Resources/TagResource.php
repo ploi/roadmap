715a8b2b9e9929fdcc41cd9cdb7b544645947de0
Nerijus <krekas@gmail.com>
2022-10-04 13:13:17 +0300

    Admins can manage tags from admin panel

---
+++ app/Filament/Resources/TagResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Spatie\Tags\Tag;
+use Filament\Tables;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use App\Filament\Resources\TagResource\Pages;
+use App\Filament\Resources\TagResource\RelationManagers;
+
+class TagResource extends Resource
+{
+    protected static ?string $model = Tag::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?string $navigationGroup = 'Manage';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make([
+                    Forms\Components\TextInput::make('name')->required(),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->sortable()
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'  => Pages\ListTags::route('/'),
+            'create' => Pages\CreateTag::route('/create'),
+            'edit'   => Pages\EditTag::route('/{record}/edit'),
+        ];
+    }
+}
