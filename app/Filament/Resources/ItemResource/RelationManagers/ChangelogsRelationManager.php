f325fb8a122251afb97bb6c9623774b82f19d2a4
Sebastiaan Kloos <sebastiaan@codecycler.com>
2022-10-24 16:41:11 +0200

    only show changelog relation manager when setting is enabled

--- app/Filament/Resources/ItemResource/RelationManagers/ChangelogsRelationManager.php
+++ app/Filament/Resources/ItemResource/RelationManagers/ChangelogsRelationManager.php
@@ -2,15 +2,22 @@
 
 namespace App\Filament\Resources\ItemResource\RelationManagers;
 
+use App\Settings\GeneralSettings;
 use Filament\Resources\Table;
 use App\Filament\Resources\ChangelogResource;
 use Filament\Resources\RelationManagers\RelationManager;
+use Illuminate\Database\Eloquent\Model;
 
 class ChangelogsRelationManager extends RelationManager
 {
     protected static string $relationship = 'changelogs';
 
     protected static ?string $recordTitleAttribute = 'title';
+
+    public static function canViewForRecord(Model $ownerRecord): bool
+    {
+        return app(GeneralSettings::class)->enable_changelog;
+    }
 
     public static function table(Table $table): Table
     {
