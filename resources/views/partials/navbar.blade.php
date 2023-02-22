965ff654096ecb6f030acbb68fad199fb7cb22f6
Dennis Smink <dennissmink@gmail.com>
2023-02-22 10:52:32 +0100

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -114,7 +114,7 @@
 
             <div id="dropdown-cta" class="p-4 mt-6 bg-gray-100 rounded-lg" role="alert">
                 <p class="text-sm text-gray-500">
-                    <a href="https://github.com/ploi-deploy/roadmap" target="_blank"
+                    <a href="https://github.com/ploi/roadmap" target="_blank"
                        class="font-semibold border-b border-dotted">Open-source</a>
                     roadmapping software by
                     <a href="https://ploi.io/?ref=roadmap" target="_blank" class="font-semibold border-b border-dotted">ploi.io</a>
