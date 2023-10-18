1d5776ef2e5013fded4facd59cb9639360f686e2
Sebastiaan Kloos <sebastiaan@codecycler.com>
2023-10-18 11:49:35 +0200

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,5 @@
-import Alpine from 'alpinejs'
-import focus from '@alpinejs/focus'
-import Tooltip from "@ryangjchandler/alpine-tooltip";
-import Clipboard from "@ryangjchandler/alpine-clipboard"
 import Tribute from "tributejs";
 import axios from 'axios';
 
 window.axios = axios;
-
-Alpine.plugin(focus)
-Alpine.plugin(Tooltip);
-Alpine.plugin(Clipboard);
-
 window.Tribute = Tribute;
-window.Alpine = Alpine
-
-Alpine.start()
-
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -15,13 +15,12 @@
         }
     </style>
 
-    @vite('resources/css/app.css')
-
     @if(file_exists($favIcon = storage_path('app/public/favicon.png')))
         <link href="{{ asset('storage/favicon.png') }}?v={{ md5_file($favIcon) }}" rel="icon" type="image/x-icon"/>
     @endif
 
     @filamentStyles
+    @vite('resources/css/app.css')
 
     @include('partials.meta')
 
