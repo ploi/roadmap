7f289340920dd595e3f1654109cda9bd31a4f30c
Cannonb4ll <dennissmink@gmail.com>
2023-11-01 13:10:47 +0100

    wip

--- app/Http/Controllers/BoardsController.php
+++ app/Http/Controllers/BoardsController.php
@@ -9,7 +9,7 @@ class BoardsController extends Controller
 {
     public function show(Project $project, Board $board)
     {
-        abort_if($project->private && !auth()->user()?->canAccessFilament(), 404);
+        abort_if($project->private && !auth()->user()?->hasAdminAccess(), 404);
 
         return view('board', [
             'project' => $project,
