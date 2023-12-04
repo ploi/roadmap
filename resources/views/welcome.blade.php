97565a8a1bc0568242a83c7f01ef260458d03cfb
Cannonb4ll <dennissmink@gmail.com>
2023-12-04 11:17:30 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,10 @@
     @endif
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
         @foreach(app(\App\Settings\GeneralSettings::class)->dashboard_items as $item)
-            <div class="space-y-2 col-span-{{ $item->column_span ?? 1 }}">
-                <h2 class="text-lg tracking-tight font-bold">{{ trans('general.'. $item->type) }}</h2>
+            <div class="space-y-2 col-span-{{ $item['column_span'] ?? 1 }}">
+                <h2 class="text-lg tracking-tight font-bold">{{ trans('general.'. $item['type']) }}</h2>
 
-                <livewire:is component="welcome.{{ $item->type }}"/>
+                <livewire:is component="welcome.{{ $item['type'] }}"/>
             </div>
         @endforeach
     </div>
