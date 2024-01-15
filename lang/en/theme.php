9fd1ed4dd498e08ff70bc076725cd4d3db3f606b
Augus <info@autive.nl>
2024-01-15 21:52:58 +0100

    Translate font-family helper text

--- lang/en/theme.php
+++ lang/en/theme.php
@@ -6,5 +6,6 @@ return [
     'logo-helper-text' => 'Make sure your storage is linked (by running php artisan storage:link).',
     'favicon' => 'Favicon',
     'font-family' => 'Font Family',
+    'font-family-helper-text' => 'Choose a font family from <a href="https://fonts.bunny.net" target="_blank" rel="noreferrer">Bunny Fonts</a> (e.g. \'Roboto\')',
     'primary-color' => 'Primary Color',
 ];
