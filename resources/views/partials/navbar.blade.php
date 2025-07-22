5ef742615241ba80e71bb36c63b3f864c3e4d8a2
Jay Greentree <215971+jaysavage1983@users.noreply.github.com>
2025-07-22 08:11:06 -0500

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -64,7 +64,9 @@
                 @endif
             </ul>
             <div>
+                {{--
                 <p class="px-2 text-lg font-semibold mb-2">{{ trans('projects.projects') }}</p>
+                --}}
                 @if($projects->count() > 0)
                     <ul class="space-y-2">
                         @foreach($projects->groupBy('group') as $group => $projects)
@@ -78,7 +80,13 @@
                                 </div>
                                 </li>
                             @endif
-
+                    <button type="button" class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                  
+                  <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>
+                  {{ trans('projects.projects') }}</span>
+                  <svg sidebar-toggle-item class="w-6 h-6" fill="#013852" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </button>
+            <ul id="dropdown-example" class="hidden py-2 space-y-2">
                             @foreach($projects as $project)
                                 <li>
                                     <a
@@ -103,6 +111,7 @@
                                     </a>
                                 </li>
                             @endforeach
+                        </ul>
                         @endforeach
                     </ul>
                 @else
