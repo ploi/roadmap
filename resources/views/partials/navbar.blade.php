7009be26df6b3636b54ad31eac3ff80947a730fc
Jay Greentree <215971+jaysavage1983@users.noreply.github.com>
2025-08-06 13:43:11 -0500

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -80,12 +80,15 @@
                                 </div>
                                 </li>
                             @endif
+{{--
+Begin Projects Dropdown                        
+--}}
                     <button type="button" class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-projects" data-collapse-toggle="dropdown-projects">
                   
                   <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>
                     {{ trans('projects.projects') }}
                   </span>
-                  <svg sidebar-toggle-item class="w-6 h-6" fill="#013852" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                  <svg sidebar-toggle-item class="w-6 h-6" fill="#000000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
             <ul id="dropdown-projects" class="hidden py-2 space-y-2">
                             @foreach($projects as $project)
@@ -113,6 +116,9 @@
                                 </li>
                             @endforeach
                         </ul>
+{{--
+End Projects Dropdown                        
+--}}
                         @endforeach
                     </ul>
                 @else
