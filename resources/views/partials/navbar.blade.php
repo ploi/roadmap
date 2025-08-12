bc3e9df8be5f36366685a47d30d86a6e5f351636
Jay Greentree <215971+jaysavage1983@users.noreply.github.com>
2025-08-12 12:46:48 -0500

    Update navbar.blade.php
    Allows dropdown to remain open on project pages

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -90,7 +90,11 @@ Begin Projects Dropdown
                   </span>
                   <svg sidebar-toggle-item class="w-6 h-6" fill="#000000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
-            <ul id="dropdown-projects" class="hidden py-2 space-y-2">
+            @if(request()->routeIs('projects') || request()->routeIs('projects.*'))
+            <ul id="dropdown-projects" class="py-2 space-y-2">
+                @else
+                <ul id="dropdown-projects" class="hidden py-2 space-y-2">
+                    @endif
                             @foreach($projects as $project)
                                 <li>
                                     <a
