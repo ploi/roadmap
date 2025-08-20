6a7f12b8289ea5044329658f7e52803b5dca620e
Mathias Palmqvist <mathias@mattitja.se>
2025-08-20 15:28:37 +0200

    Translation

--- lang/sv/auth.php
+++ lang/sv/auth.php
@@ -13,6 +13,6 @@
 
 return [
     'failed'   => 'Dessa uppgifter stämmer inte överens med vårt register.',
-    'password' => 'The provided password is incorrect.',
     'throttle' => 'För många inloggningsförsök. Var vänlig försök igen om :seconds sekunder.',
+    "password" => "Det angivna lösenordet är felaktigt.",
 ];
