881896875d2452d8336c7d79a4d8fa17ad7037ed
Jay Greentree <215971+jaysavage1983@users.noreply.github.com>
2025-09-28 09:42:51 -0500

    Update navbar.blade.php
    Fixed dropdown so it stays open on 'project' pages

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -96,7 +96,7 @@
                   {{ trans('projects.projects') }}</span>
                   <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
-            <ul id="dropdown-projects" class="hidden py-2 space-y-2">
+            <ul id="dropdown-projects" class="{{ Request::is('projects/*') ? '' : 'hidden' }} py-2 space-y-2">
                             @foreach($projects as $project)
                                 <li>
                                     <a
