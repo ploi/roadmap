8eae1a90535704d62b2a6b0e18b387ea61728fb2
Sebastiaan Kloos <sebastiaan@codecycler.com>
2025-11-28 11:03:18 +0100

    refactor(search): remove legacy search modal and components
    Remove old search modal implementation and search button label
    component that are no longer needed after migration to spotlight
    search. These files have been replaced by the new spotlight search
    functionality implemented in previous commits.
    
    Deleted files:
    - resources/views/modals/search.blade.php
    - resources/views/components/search-button-label.blade.php

--- resources/views/components/search-button-label.blade.php
+++
@@ -1,6 +0,0 @@
-<span class="inline-flex items-center gap-2">
-    <span>Search</span>
-    <kbd class="hidden sm:inline-flex items-center gap-0.5 px-2 py-1 text-xs font-semibold rounded bg-white/10 text-white/80 border border-white/20">
-        <span class="text-[11px]">⌘</span>K
-    </kbd>
-</span>
